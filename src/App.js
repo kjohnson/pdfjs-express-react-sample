67d0e161fb7ec39af009339ab16c03c4cae317f4
Kyle Johnson <kbjohnson90+github@gmail.com>
2021-02-03 16:19:27 -0500

    Update with simplified W4 demo.

--- src/App.js
+++ src/App.js
@@ -10,33 +10,32 @@ const App = () => {
     WebViewer(
       {
         path: '/webviewer/lib',
-        initialDoc: '/files/pdftron_about.pdf',
+        initialDoc: '/files/w4.pdf',
+        disabledElements: [ // https://pdfjs.express/documentation/ui-customization/hiding-elements
+          // Header
+          'leftPanelButton',
+          'viewControlsButton',
+          'freeHandToolGroupButton',
+          'textToolGroupButton',
+          'shapeToolGroupButton',
+          'freeTextToolButton',
+          'stickyToolButton',
+          'miscToolGroupButton',
+          // Popups
+          'annotationPopup',
+          'highlightToolButton',
+          'freeHandToolButton',
+        ]
       },
       viewer.current,
     ).then((instance) => {
-      const { docViewer, Annotations } = instance;
-      const annotManager = docViewer.getAnnotationManager();
-
-      docViewer.on('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation();
-        rectangleAnnot.PageNumber = 1;
-        // values are in page coordinates with (0, 0) in the top left
-        rectangleAnnot.X = 100;
-        rectangleAnnot.Y = 150;
-        rectangleAnnot.Width = 200;
-        rectangleAnnot.Height = 50;
-        rectangleAnnot.Author = annotManager.getCurrentUser();
-
-        annotManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotManager.redrawAnnotation(rectangleAnnot);
-      });
+      // ...
     });
   }, []);
 
   return (
     <div className="App">
-      <div className="header">React sample</div>
+      <div className="header">W4 Demo</div>
       <div className="webviewer" ref={viewer}></div>
     </div>
   );
